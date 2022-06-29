c61e7934921fb838d802b199be8cf52a8156e539
aka <akatsukisatoshi@gmail.com>
2022-06-29 22:41:11 +0900

    default exportをやめる

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 
-function App() {
+export function App() {
   const [count, setCount] = useState(0);
 
   return (
@@ -35,5 +35,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
